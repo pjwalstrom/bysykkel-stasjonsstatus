8b1430048f678ee29e89ea7eb2e3558ca1025736
perwalst <per.jorgen.walstrom@finn.no>
2019-11-11 21:14:16 +0100

    test to verify that a Station is returned from Resource

--- src/main/java/com/pj/BysykkelResource.java
+++ src/main/java/com/pj/BysykkelResource.java
@@ -6,25 +6,22 @@ import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
-import java.util.Collection;
+import java.util.stream.Collectors;
 
-@Path("/station")
+@Path("/")
 @Produces(MediaType.TEXT_PLAIN + ";charset=utf-8")
 public class BysykkelResource {
     static final String ERROR_MSG_NO_STATION_FOUND = "No station with id ";
 
     @GET
-    public Response getAll() {
-        StringBuilder result = new StringBuilder();
-        Collection<Station> stations = BysykkelServer.STATIONS.values();
-        for (Station station : stations) {
-            result.append(station.toString()).append("\n");
-        }
-        return Response.status(Response.Status.OK).entity(result.toString()).build();
+    @Path("stations")
+    public Response getStations() {
+        String result = BysykkelServer.STATIONS.values().stream().map(Station::toString).collect(Collectors.joining("\n"));
+        return Response.status(Response.Status.OK).entity(result).build();
     }
 
     @GET
-    @Path("/{id}")
+    @Path("stations/{id}")
     public Response getStation(@PathParam("id") String id) {
         if (!BysykkelServer.STATIONS.containsKey(id)) {
             return Response.status(Response.Status.NOT_FOUND).entity(ERROR_MSG_NO_STATION_FOUND + id).build();
--- src/test/groovy/com/pj/BysykkelResourceSpec.groovy
+++ src/test/groovy/com/pj/BysykkelResourceSpec.groovy
@@ -14,10 +14,11 @@ class BysykkelResourceSpec extends Specification {
     private HttpServer server
     @Shared
     private WebTarget target
+    private final static Station STATION = new Station('100', 'Mordor', 1, 0)
     private static final String BASE_URL = 'http://localhost:8080/foo/'
 
     void setupSpec() {
-        server = BysykkelServer.startServer(BASE_URL, [:])
+        server = BysykkelServer.startServer(BASE_URL, [(STATION.id):STATION])
         target = ClientBuilder.newClient().target(BASE_URL)
     }
 
@@ -25,9 +26,13 @@ class BysykkelResourceSpec extends Specification {
         server.shutdownNow()
     }
 
-    def 'calling valid resource results in a HTTP-status 200/OK'() {
-        expect:
-        target.path("station").request().get(Response.class).status == Response.Status.OK.statusCode
+    def 'calling valid resource results in a HTTP-status 200/OK with a Station'() {
+        when:
+        Response response = target.path("stations").request().get(Response.class)
+
+        then:
+        response.status == Response.Status.OK.statusCode
+        response.readEntity(String.class).contains(STATION.toString())
     }
 
     def 'calling missing resource results in a HTTP-status 404/NOT FOUND'() {
@@ -37,7 +42,7 @@ class BysykkelResourceSpec extends Specification {
 
     def 'calling valid resource but with missing id results in a HTTP-status 404/NOT FOUND but with a customized error message'() {
         when:
-        Response response = target.path("station/none").request().get(Response.class)
+        Response response = target.path("stations/none").request().get(Response.class)
 
         then:
         response.status == Response.Status.NOT_FOUND.statusCode
