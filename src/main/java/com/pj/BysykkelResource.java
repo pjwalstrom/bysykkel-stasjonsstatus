c2a20bd680903675f5e29723b8931fc375cb95b9
perwalst <per.jorgen.walstrom@finn.no>
2019-11-14 12:53:23 +0100

    Resource now produces json instead of plain text

--- src/main/java/com/pj/BysykkelResource.java
+++ src/main/java/com/pj/BysykkelResource.java
@@ -1,31 +1,39 @@
 package com.pj;
 
+import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
-import java.util.stream.Collectors;
+
+import static com.pj.BysykkelServer.STATIONS;
 
 @Path("/")
-@Produces(MediaType.TEXT_PLAIN + ";charset=utf-8")
+@Produces(MediaType.APPLICATION_JSON + ";charset=utf-8")
 public class BysykkelResource {
     static final String ERROR_MSG_NO_STATION_FOUND = "No station with id ";
+    private static final Gson GSON = new Gson();
 
     @GET
     @Path("stations")
     public Response getStations() {
-        String result = BysykkelServer.STATIONS.values().stream().map(Station::toString).collect(Collectors.joining("\n"));
-        return Response.status(Response.Status.OK).entity(result).build();
+        JsonArray jsonArray = new JsonArray();
+        for (Station station : STATIONS.values()) {
+            jsonArray.add(GSON.toJsonTree(station));
+        }
+        return Response.status(Response.Status.OK).entity(GSON.toJson(jsonArray)).build();
     }
 
     @GET
     @Path("stations/{id}")
     public Response getStation(@PathParam("id") String id) {
-        if (!BysykkelServer.STATIONS.containsKey(id)) {
+        if (!STATIONS.containsKey(id)) {
             return Response.status(Response.Status.NOT_FOUND).entity(ERROR_MSG_NO_STATION_FOUND + id).build();
         }
-        return Response.status(Response.Status.OK).entity(BysykkelServer.STATIONS.get(id).toString()).build();
+        return Response.status(Response.Status.OK).entity(GSON.toJson(STATIONS.get(id))).build();
     }
 }
--- src/test/groovy/com/pj/BysykkelResourceSpec.groovy
+++ src/test/groovy/com/pj/BysykkelResourceSpec.groovy
@@ -1,5 +1,7 @@
 package com.pj
 
+import com.google.gson.Gson
+import groovy.json.JsonSlurper
 import org.glassfish.grizzly.http.server.HttpServer
 import spock.lang.Shared
 import spock.lang.Specification
@@ -14,11 +16,12 @@ class BysykkelResourceSpec extends Specification {
     HttpServer server
     @Shared
     WebTarget target
-    static final Station STATION = new Station('100', 'Mordor', 1, 0)
+    static final Station STATION1 = new Station('1', 'Mordor', 1, 0)
+    static final Station STATION2 = new Station('2', 'Isengard', 0, 1)
     static final String BASE_URL = 'http://localhost:8080/foo/'
 
     def setupSpec() {
-        server = BysykkelServer.startServer(BASE_URL, [(STATION.id): STATION])
+        server = BysykkelServer.startServer(BASE_URL, [(STATION1.id): STATION1, (STATION2.id): STATION2])
         target = ClientBuilder.newClient().target(BASE_URL)
     }
 
@@ -26,13 +29,29 @@ class BysykkelResourceSpec extends Specification {
         server.shutdownNow()
     }
 
-    def 'calling valid resource results in a HTTP-status 200/OK with a Station'() {
+    def 'calling stations resource results in a HTTP-status 200/OK with a Station'() {
         when:
         Response response = target.path("stations").request().get(Response.class)
 
         then:
         response.status == Response.Status.OK.statusCode
-        response.readEntity(String.class).contains(STATION.toString())
+        String entity = response.readEntity(String.class)
+        entity.contains(new Gson().toJson(STATION1))
+
+        and: 'json is valid'
+        new JsonSlurper().parseText(entity)
+    }
+
+    def 'calling station/id resource results in a HTTP-status 200/OK with a Station with valid json'() {
+        given:
+        JsonSlurper jsonSlurper = new JsonSlurper()
+
+        when:
+        Response response = target.path("stations/" + STATION1.id).request().get(Response.class)
+
+        then:
+        response.status == Response.Status.OK.statusCode
+        jsonSlurper.parseText(response.readEntity(String.class)) == jsonSlurper.parseText(new Gson().toJson(STATION1))
     }
 
     def 'calling missing resource results in a HTTP-status 404/NOT FOUND'() {
